ca6260fbebe9563aec557e41ab515607df4716e8
Gabriel Assana <gabrielhhz2@gmail.com>
2022-03-08 23:35:23 -0300

    Subindo Gif pra Readme.md

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -26,20 +26,19 @@ export default function SideBar({ handleClickButton,selectedGenreId }: SidebarPr
 
   return(
     <nav className="sidebar">
-    <span>Watch<p>Me</p></span>
+      <span>Watch<p>Me</p></span>
 
-    <div className="buttons-container">
-      {genres.map(genre => (
-        <Button
-          key={String(genre.id)}
-          title={genre.title}
-          iconName={genre.name}
-          onClick={() => handleClickButton(genre.id)}
-          selected={selectedGenreId === genre.id}
-        />
-      ))}
-    </div>
-
-  </nav>
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
   )
 }
